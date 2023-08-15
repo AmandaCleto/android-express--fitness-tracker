77de692bc619ae089e83dd5e05eda41f45259d4f
amanda cleto <amanda.cleto.dev@gmail.com>
2023-08-15 15:08:46 -0300

    update: create a style file to componentize styles

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -1,11 +1,26 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnImc: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnImc = findViewById(R.id.btn_imc)
+        btnImc.setOnClickListener {
+            // NAVEGATION
+            // intent is class that you use when you want to use a service from Android
+            // it's very used for navigation
+
+            //when we want to pass the class [page] -someClass- will instantiate for us, we use ::class.java, passing the reference to it.
+            val intent = Intent(this, ImcActivity::class.java)
+            startActivity(intent) //opens the second page
+        }
     }
 }
