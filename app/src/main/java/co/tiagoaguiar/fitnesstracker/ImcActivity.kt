07b2af483d250b2dd39c3594251779695e22b226
amanda cleto <amanda.cleto.dev@gmail.com>
2023-08-15 15:34:30 -0300

    update: create logic to show imc response

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -3,9 +3,12 @@ package co.tiagoaguiar.fitnesstracker
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.constraintlayout.motion.widget.TransitionBuilder.validate
 
 class ImcActivity : AppCompatActivity() {
     private lateinit var editWeight: EditText
@@ -24,7 +27,35 @@ class ImcActivity : AppCompatActivity() {
                 Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
                 return@setOnClickListener //this means that this bloc of code will be stopped
             }
+
+            val weight = editWeight.text.toString().toInt();
+            val height = editHeight.text.toString().toInt();
+
+            val resultImc = calculateImc(weight, height)
+            val imcResponseId = imcResponse(resultImc);
+
+            Toast.makeText(this, imcResponseId, Toast.LENGTH_SHORT).show()
         }
+    }
+
+    /*
+    * when returning a R class, you must return a Int
+    * because R class returns the reference of whatever you are returning,
+    * and that reference is an int
+    * */
+
+    @StringRes //means to developer that this function shall return a R.string
+    private fun imcResponse(imc: Double): Int {
+       return when {
+           imc < 15.0 -> R.string.imc_severely_low_weight
+           imc < 16.0 -> R.string.imc_very_low_weight
+           imc < 18.5 -> R.string.imc_low_weight
+           imc < 25.0 -> R.string.normal
+           imc < 30.0 -> R.string.imc_high_weight
+           imc < 35.0 -> R.string.imc_so_high_weight
+           imc < 40.0 -> R.string.imc_severely_high_weight
+           else -> R.string.imc_extreme_weight
+       }
     }
 
     private fun validate(): Boolean {
@@ -32,5 +63,10 @@ class ImcActivity : AppCompatActivity() {
                 && !editWeight.text.toString().startsWith("0") && !editHeight.text.toString()
             .startsWith("0")
                 )
+    }
+
+    private fun calculateImc(weight: Int, height: Int): Double {
+        return weight / ((height / 100.0) * (height / 100.0))
+
     }
 }
