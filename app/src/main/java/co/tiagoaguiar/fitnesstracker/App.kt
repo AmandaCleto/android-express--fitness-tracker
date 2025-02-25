58cd66f60d910da7fb098acd08ec64b9067d5535
AmandaCleto <amanda.cleto.dev@gmail.com>
2025-02-24 21:38:12 -0300

    feat: adding application class

---
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/App.kt
@@ -0,0 +1,12 @@
+package co.tiagoaguiar.fitnesstracker
+
+import android.app.Application
+
+
+// executes along with the initialization of the app
+// must be add in android manifest
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
